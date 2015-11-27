91cd746fa1c5650bb23d57fbe44dcc5607e856f7
MareesAT <andriestm@hotmail.com>
2015-11-27 21:09:59 +0100

    toevoegen van mijn  scripts

---
+++ GWAS_tutorial/heterozygosity_outliers_list.R
@@ -0,0 +1,5 @@
+het <- read.table("R_check.het", head=TRUE)
+het$HET_RATE = (het$"N.NM." - het$"O.HOM.")/het$"N.NM."
+het_fail = subset(het, (het$HET_RATE < mean(het$HET_RATE)-3*sd(het$HET_RATE)) | (het$HET_RATE > mean(het$HET_RATE)+3*sd(het$HET_RATE)));
+het_fail$HET_DST = (het_fail$HET_RATE-mean(het$HET_RATE))/sd(het$HET_RATE);
+write.table(het_fail, "fail-het-qc.txt", row.names=FALSE)
