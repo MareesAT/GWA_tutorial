aee263eedb8c5682dab2648ae1a5aa694d17d4ff
MareesAT <andriestm@hotmail.com>
2015-12-07 08:59:20 +0100

    Remove dubbel

--- GWAS_tutorial/MAF_check.R
+++
@@ -1,6 +0,0 @@
-maf_freq <- read.table("MAF_check.frq", header =TRUE, as.is=T)
-pdf("MAF_distribution.pdf")
-hist(maf_freq[,5],main = "MAF distribution", xlab = "MAF")
-dev.off()
-
-
--- GWAS_tutorial/plot_mds.R
+++
@@ -1,5 +0,0 @@
-a <- read.table("mds.mds", header = TRUE)
-
-pdf("pop_strat.pdf")
-plot(a$C1,a$C2, xlab= "PC1", ylab = "PC2", main = "population stratification")
-dev.off()
