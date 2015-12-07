3e66832f44472a8ce655ef21ecc9e4583e176d65
MareesAT <andriestm@hotmail.com>
2015-12-07 08:47:01 +0100

    Remove duplicated directory

--- GWAS_tutorial/check_heterozygosity_rate.R
+++
@@ -1,5 +0,0 @@
-het <- read.table("R_check.het", head=TRUE)
-pdf("hetrozygosity.pdf")
-het$HET_RATE = (het$"N.NM." - het$"O.HOM.")/het$"N.NM."
-hist(het$HET_RATE, xlab="Hetrozygosity Rate", ylab="Frequency", main= "Heterozygosity Rate")
-dev.off()
